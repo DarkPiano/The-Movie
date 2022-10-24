82a1e632f316b7cdb022ff6eb6907c88331b55cf
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-10-24 14:16:42 +0900

    feat: creating a bottomNavigationBar

--- lib/widgets/main screen/main_screen_widget.dart
+++ lib/widgets/main screen/main_screen_widget.dart
@@ -8,13 +8,28 @@ class MainScreenWidget extends StatefulWidget {
 }
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('TMDB'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movie',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'Series',
+          ),
+        ],
       ),
     );
   }
