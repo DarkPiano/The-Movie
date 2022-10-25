850e8978abfaf4a4b844043b378ad35959fad62a
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-10-25 14:16:56 +0900

    feat: creating a local database

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -21,7 +21,73 @@ class MovieListWidget extends StatelessWidget {
       imageName: AppImages.fall,
       title: 'Fall',
       time: 'August 10, 2022',
-      subtitle: 'For best friends Becky (Grace Caroline Currey) and Hunter (Virginia Gardner), life is all about conquering fears and pushing limits. But after they climb 2,000 feet to the top of a remote, abandoned radio tower, they find themselves stranded with no way down. Now Becky and Hunter’s expert climbing skills will be put to the ultimate test as they desperately fight to survive the elements, a lack of supplies, and vertigo-inducing heights in this adrenaline-fueled thriller co-starring Jeffrey Dean Morgan.',
+      subtitle: 'For best friends Becky (Grace Caroline Currey) and Hunter '
+          '(Virginia Gardner), life is all about conquering fears and pushing '
+          'limits. But after they climb 2,000 feet to the top of a remote, '
+          'abandoned radio tower, they find themselves stranded with no way '
+          'down. Now Becky and Hunter’s expert climbing skills will be put to '
+          'the ultimate test as they desperately fight to survive the elements,'
+          ' a lack of supplies, and vertigo-inducing heights in this '
+          'adrenaline-fueled thriller co-starring Jeffrey Dean Morgan.',
+    ),
+    Movie(
+        imageName: AppImages.fullmetalAlchemistKanketsuhenFukushushaScarP1,
+        title: 'Fullmetal Alchemist: Final Chapter - The Last Transmutation',
+        time: 'June 24, 2022',
+        subtitle: 'The Elric brothers long and winding journey comes to a '
+            'close in this epic finale, where they must face off against an '
+            'unworldly, nationwide threat.',
+    ),
+    Movie(
+      imageName: AppImages.blackAdam,
+      title: 'Black Adam',
+      time: 'October 19, 2022',
+      subtitle: 'Nearly 5,000 years after he was bestowed with the almighty '
+          'powers of the Egyptian gods-and imprisoned just as quickly-Black '
+          'Adam is freed from his earthly tomb, ready to unleash his unique '
+          'form of justice on the modern world.',
+    ),
+    Movie(
+      imageName: AppImages.thePeripheral,
+      title: 'The Peripheral',
+      time: 'October 21, 2022',
+      subtitle: 'Set in the future when technology has subtly altered society,'
+          ' a woman discovers a secret connection to an alternate reality as '
+          'well as a dark future of her own.',
+    ),
+    Movie(
+      imageName: AppImages.theBatman,
+      title: 'The Batman',
+      time: 'March 1, 2022',
+      subtitle: 'When a sadistic serial killer begins murdering key political '
+          'figures in Gotham, Batman is forced to investigate the citys hidden '
+          'corruption and question his familys involvement.',
+    ),
+    Movie(
+      imageName: AppImages.doctorStrangeInTheMultiverseOfMadness,
+      title: 'Doctor Strange in the Multiverse of Madness',
+      time: 'May 2, 2022',
+      subtitle: 'Doctor Strange teams up with a mysterious teenage girl from '
+          'his dreams who can travel across multiverses, to battle multiple '
+          'threats, including other-universe versions of himself, which '
+          'threaten to wipe out millions across the multiverse. They seek '
+          'help from Wanda the Scarlet Witch, Wong and others.',
+    ),
+    Movie(
+        imageName: AppImages.avatarTheWayOfWater,
+        title: 'Avatar: The Way of Water',
+        time: 'December 14, 2022',
+        subtitle: 'Jake Sully lives with his newfound family formed on the '
+            'planet of Pandora. Once a familiar threat returns to finish what '
+            'was previously started, Jake must work with Neytiri and the army '
+            'of the Navi race to protect their planet.',
+    ),
+    Movie(
+        imageName: AppImages.blackPantherWakandaForever,
+        title: 'Black Panther: Wakanda Forever',
+        time: 'November 14, 2022',
+        subtitle: 'The nation of Wakanda is pitted against intervening world '
+            'powers as they mourn the loss of their king TChalla.',
     ),
   ];
 
