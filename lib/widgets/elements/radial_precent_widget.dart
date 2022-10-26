232a7c2459418aa967128ff9247c7839e8ce56e8
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-10-26 15:23:44 +0900

    feat: adding an interface to a RadialPersentWdget

--- lib/widgets/elements/radial_precent_widget.dart
+++ lib/widgets/elements/radial_precent_widget.dart
@@ -12,7 +12,19 @@ class RadialPersentWdget extends StatelessWidget {
           width: 100,
           height: 100,
           decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-          child: RadialPercentWidget(),
+          child: RadialPercentWidget(
+            percent: 0.72,
+            fillColor: Colors.black,
+            lineColor: Colors.green,
+            freeColor: Colors.grey,
+            lineWidget: 5,
+            child: Text(
+              '72%',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
         ),
       ),
     );
@@ -20,35 +32,76 @@ class RadialPersentWdget extends StatelessWidget {
 }
 
 class RadialPercentWidget extends StatelessWidget {
-  const RadialPercentWidget({Key? key}) : super(key: key);
+  final Widget child;
+
+  final double percent;
+  final Color fillColor;
+  final Color lineColor;
+  final Color freeColor;
+  final double lineWidget;
+
+  const RadialPercentWidget({
+    Key? key,
+    required this.child,
+    required this.percent,
+    required this.fillColor,
+    required this.lineColor,
+    required this.freeColor,
+    required this.lineWidget,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       fit: StackFit.expand,
       children: [
-        CustomPaint(painter: MyPainter()),
+        CustomPaint(
+            painter: MyPainter(
+          percent: percent,
+          fillColor: fillColor,
+          lineColor: lineColor,
+          freeColor: freeColor,
+          lineWidget: lineWidget,
+        )),
+        Padding(
+          padding: const EdgeInsets.all(11.0),
+          child: Center(
+            child: child,
+          ),
+        ),
       ],
     );
   }
 }
 
-
 class MyPainter extends CustomPainter {
-  final double percent = 0.72;
+  final double percent;
+  final Color fillColor;
+  final Color lineColor;
+  final Color freeColor;
+  final double lineWidget;
+
+  MyPainter({
+    required this.percent,
+    required this.fillColor,
+    required this.lineColor,
+    required this.freeColor,
+    required this.lineWidget,
+  });
+
   @override
   void paint(Canvas canvas, Size size) {
     final backgroundPaint = Paint();
-    backgroundPaint.color = Colors.black;
+    backgroundPaint.color = fillColor;
     backgroundPaint.style = PaintingStyle.fill;
     canvas.drawOval(Offset.zero & size, backgroundPaint);
 
     final filedPaint = Paint();
-    filedPaint.color = Colors.red;
+    filedPaint.color = freeColor;
     filedPaint.style = PaintingStyle.stroke;
-    filedPaint.strokeWidth = 5;
+    filedPaint.strokeWidth = lineWidget;
     canvas.drawArc(
-      Offset(5.5, 5.5) & Size(size.width - 11, size.height- 11),
+      Offset(5.5, 5.5) & Size(size.width - 11, size.height - 11),
       pi * 2 * percent - (pi / 2),
       pi * 2 * (1.0 - percent),
       false,
@@ -56,12 +109,12 @@ class MyPainter extends CustomPainter {
     );
 
     final feelPaint = Paint();
-    feelPaint.color = Colors.green;
+    feelPaint.color = lineColor;
     feelPaint.style = PaintingStyle.stroke;
-    feelPaint.strokeWidth = 5;
+    feelPaint.strokeWidth = lineWidget;
     feelPaint.strokeCap = StrokeCap.round;
     canvas.drawArc(
-      Offset(5.5, 5.5) & Size(size.width - 11, size.height- 11),
+      Offset(5.5, 5.5) & Size(size.width - 11, size.height - 11),
       -pi / 2,
       pi * 2 * percent,
       false,
