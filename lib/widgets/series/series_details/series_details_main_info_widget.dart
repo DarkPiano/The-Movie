502c794c04ac6d0ba3ec68696699b5529c78941b
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-10-27 14:28:04 +0900

    feat: create _TopPosterWidget

--- lib/widgets/series/series_details/series_details_main_info_widget.dart
+++ lib/widgets/series/series_details/series_details_main_info_widget.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:the_novie_db/resources/resources.dart';
 
 class SeriesDetailsMainInfoWidget extends StatelessWidget {
   const SeriesDetailsMainInfoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: const [
+        _TopPosterWidget(),
+      ],
+    );
   }
 }
 
+class _TopPosterWidget extends StatelessWidget {
+  const _TopPosterWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        Image(image: AssetImage(AppImages.cyberpunkTopHeader)),
+        Positioned(
+          top: 20,
+          left: 20,
+          bottom: 20,
+          child: Image(image: AssetImage(AppImages.cyberpunk)),
+        ),
+      ],
+    );
+  }
+}
