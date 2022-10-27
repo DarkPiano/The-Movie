a3bdcaec7cd7d4b5adf291daac406dc9ac0dc25b
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-10-27 14:15:55 +0900

    feat: create SeriesDetailsWidget

--- lib/widgets/series/series_details/series_details_widget.dart
+++ lib/widgets/series/series_details/series_details_widget.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class SeriesDetailsWidget extends StatefulWidget {
-  const SeriesDetailsWidget({Key? key}) : super(key: key);
+  final int seriesId;
+
+  const SeriesDetailsWidget({
+    Key? key,
+    required this.seriesId,
+  }) : super(key: key);
 
   @override
   State<SeriesDetailsWidget> createState() => _SeriesDetailsWidgetState();
@@ -10,6 +15,20 @@ class SeriesDetailsWidget extends StatefulWidget {
 class _SeriesDetailsWidgetState extends State<SeriesDetailsWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Cyberpunk: Edgerunners'),
+      ),
+      body: ColoredBox(
+        color: const Color.fromRGBO(24, 23, 27, 1.0),
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          children: const [
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
   }
 }
