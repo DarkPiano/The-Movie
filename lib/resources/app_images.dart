16d032d289238e30511ae588a74152441a07191c
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-10-27 12:30:17 +0900

    fix: SeriesListWidget

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -5,16 +5,25 @@ class AppImages {
 
   static const String avatarTheWayOfWater =
       'images/Avatar_the_way_of_water.jpg';
+  static const String bienvenidosAEden = 'images/Bienvenidos_a_eden.jpg';
   static const String blackAdam = 'images/Black_Adam.jpg';
   static const String blackPantherWakandaForever =
       'images/Black_panther_Wakanda_forever.jpg';
+  static const String cyberpunk = 'images/Cyberpunk.jpg';
+  static const String cyberpunkTopHeader = 'images/Cyberpunk_topHeader.jpg';
+  static const String dahmer = 'images/Dahmer.jpg';
   static const String doctorStrangeInTheMultiverseOfMadness =
       'images/Doctor_Strange_in_the_multiverse_of_madness.jpg';
+  static const String echoes = 'images/Echoes.jpg';
   static const String fall = 'images/Fall.jpg';
   static const String fallTopHeader = 'images/Fall_topHeader.jpg';
+  static const String firstKill = 'images/First_kill.jpg';
   static const String fullmetalAlchemistKanketsuhenFukushushaScarP1 =
       'images/Fullmetal_alchemist-_kanketsuhen_fukushusha_scar-p1.jpg';
+  static const String houseOfTheDragon = 'images/House_of_the_dragon.jpg';
   static const String theBatman = 'images/The_batman.jpg';
+  static const String theLordOfTheRings = 'images/The_lord_of_the_rings.jpg';
   static const String thePeripheral = 'images/The_peripheral.jpg';
+  static const String theSandman = 'images/The_sandman.jpg';
   static const String actor = 'images/actor.png';
 }
--- lib/widgets/series/series_list/series_list_widget.dart
+++ lib/widgets/series/series_list/series_list_widget.dart
@@ -22,13 +22,78 @@ class _SeriesListWidgetState extends State<SeriesListWidget> {
   final _series = [
     Series(
       id: 1,
-      imageName: AppImages.fall,
+      imageName: AppImages.cyberpunk,
       title: 'Cyberpunk: Edgerunners',
-      time: '2022',
+      time: 'September 13, 2022',
       subtitle: 'A Street Kid trying to survive in a technology and body '
                'modification-obsessed city of the future. Having everything '
                'to lose, he chooses to stay alive by becoming an Edgerunner, '
                'a Mercenary outlaw also known as a Cyberpunk.',
+    ),
+    Series(
+      id: 2,
+      imageName: AppImages.firstKill,
+      title: 'First Kill',
+      time: 'June 10, 2022',
+      subtitle: 'Falling in love is tricky for teens Juliette and Calliope: '
+          'One`s a vampire, the other`s a vampire hunter and both are ready '
+          'to make their first kill.',
+    ),
+    Series(
+      id: 3,
+      imageName: AppImages.theSandman,
+      title: 'The Sandman',
+      time: 'August 5, 2022',
+      subtitle: 'Upon escaping after decades of imprisonment by a mortal '
+          'wizard, Dream, the personification of dreams, sets about to reclaim '
+          'his lost equipment.',
+    ),
+    Series(
+      id: 4,
+      imageName: AppImages.houseOfTheDragon,
+      title: 'House of the Dragon',
+      time: 'August 21,2022',
+      subtitle: 'An internal succession war within House Targaryen at the '
+          'height of its power, 172 years before the birth of Daenerys '
+          'Targaryen.',
+    ),
+    Series(
+      id: 5,
+      imageName: AppImages.bienvenidosAEden,
+      title: 'Bienvenidos a Edén',
+      time: 'May 6, 2022',
+      subtitle: 'A group of young adults attend a party on a remote island, '
+          'but the tempting paradise they find waiting for them holds dangerous '
+          'secrets and entrapments.',
+    ),
+    Series(
+      id: 6,
+      imageName: AppImages.theLordOfTheRings,
+      title: 'The Lord of the Rings: The Rings of Power',
+      time: 'September 1, 2022',
+      subtitle: 'Epic drama set thousands of years before the events of J.R.R. '
+          'Tolkien`s `The Hobbit` and `The Lord of the Rings` follows an '
+          'ensemble cast of characters, both familiar and new, as they '
+          'confront the long-feared re-emergence of evil to Middle-earth.',
+    ),
+    Series(
+      id: 7,
+      imageName: AppImages.dahmer,
+      title: 'Dahmer - Monster: The Jeffrey Dahmer Story',
+      time: 'September 21, 2022',
+      subtitle: 'Story of the Milwaukee Monster told from the perspective of '
+          'the victims and police incompetence that allowed the Wisconsin '
+          'native to go on a multiyear killing spree.',
+    ),
+    Series(
+      id: 8,
+      imageName: AppImages.echoes,
+      title: 'Echoes',
+      time: 'August 21, 2022',
+      subtitle: 'Leni and Gina are identical twins who have secretly swapped '
+          'their lives since they were children, culminating in a double life '
+          'as adults, but one of the sisters goes missing and everything in '
+          'their perfectly schemed world turns into chaos.',
     ),
   ];
 
