b643c3319646aa8a74d4f4b304c06d21a59cc8a6
DarkPiano <88538574+DarkPiano@users.noreply.github.com>
2022-11-03 11:51:36 +0900

    feat: widget inherited_helper

--- lib/library/widgets/inherited/provider.dart
+++ lib/library/widgets/inherited/provider.dart
@@ -26,3 +26,34 @@ class NotifierProvider<Model extends ChangeNotifier> extends InheritedNotifier {
     return widget is NotifierProvider<Model> ? widget.model : null;
   }
 }
+
+class Provider<Model> extends InheritedNotifier {
+  final Model model;
+
+  const Provider({
+    Key? key,
+    required this.model,
+    required Widget child,
+  }) : super(
+          key: key,
+          child: child,
+        );
+
+  static Model? watch<Model>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<Provider<Model>>()
+        ?.model;
+  }
+
+  static Model? read<Model>(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<Provider<Model>>()
+        ?.widget;
+    return widget is Provider<Model> ? widget.model : null;
+  }
+
+  @override
+  bool updateShouldNotify(Provider oldWidget) {
+    return model != oldWidget.model;
+  }
+}
